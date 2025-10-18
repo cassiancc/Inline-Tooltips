b3c8cecd2c61ba5177954846d24784a71432570b
Cassian Godsted <cassian@godsted.com>
2025-10-18 01:23:48 -0400

    reorganization

--- src/main/java/cc/cassian/inline_tooltips/InlineTooltips.java
+++ src/main/java/cc/cassian/inline_tooltips/InlineTooltips.java
@@ -46,74 +46,19 @@ public class InlineTooltips {
 
 
     public static void addTooltips(ItemStack itemStack, Item.TooltipContext tooltipContext, TooltipFlag tooltipFlag, List<Component> list) {
+        // Add icon tooltips
         if (!ModHelpers.hasShiftDown()) {
             var component = Component.empty();
             // Attribute Modifiers
-            if (CONFIG.iconTooltips.attributeTooltips) {
-                for (EquipmentSlotGroup equipmentSlotGroup : EquipmentSlotGroup.values()) {
-                    //? if >1.21.8 {
-                    itemStack.forEachModifier(equipmentSlotGroup, (holder, attributeModifier, display) -> {
-                        //?} else {
-                    /*itemStack.forEachModifier(equipmentSlotGroup, (holder, attributeModifier) -> {
-                        *///?}
-                        //? if >1.21.8
-                        if (display != ItemAttributeModifiers.Display.hidden()) {
-                            var player = Minecraft.getInstance().player;
-                            AtomicReference<Double> amount = new AtomicReference<>(attributeModifier.amount());
-                            if (player != null) {
-                                amount.set(switch (attributeModifier.operation()) {
-                                    case ADD_VALUE -> attributeModifier.amount() + player.getAttributeBaseValue(holder);
-                                    case ADD_MULTIPLIED_BASE, ADD_MULTIPLIED_TOTAL ->
-                                            attributeModifier.amount() * player.getAttributeBaseValue(holder);
-                                });
-                            }
-                            amount.set(SharpnessHelpers.addSharpnessDamage(itemStack, amount.get(), player, attributeModifier));
-                            var icon = holder.unwrapKey().orElseThrow().location();
-                            if (amount.get()!=0)
-                                addIcon(icon, amount.get(), tooltipFlag, list, component);
-                        //? if >1.21.8
-                        }
-                    });
-                }
-            }
-            if (itemStack.has(DataComponents.BEES) && CONFIG.iconTooltips.beesTooltip) {
-                var bees = itemStack.get(DataComponents.BEES);
-                if (bees == null) return;
-                addIcon(ResourceLocation.withDefaultNamespace("bees"), bees
-                        //? if >1.21.8
-                        .bees()
-                        .size(), tooltipFlag, list, component);
-
-            }
-            if (CONFIG.iconTooltips.fuelTooltip) {
-                var level = Minecraft.getInstance().level;
-                if (level != null &&
-                        //? if >1.21.8 {
-                        level.fuelValues()
-                        //?} else {
-                        /*AbstractFurnaceBlockEntity
-                        *///?}
-                                .isFuel(itemStack)) {
-                    addIcon(ResourceLocation.withDefaultNamespace("fuel"),
-                            //? if >1.21.8 {
-                            level.fuelValues().burnDuration(itemStack)
-                             //?} else {
-                            /*AbstractFurnaceBlockEntity.getFuel().get(itemStack.getItem())
-                            *///?}
-                             /200f, tooltipFlag, list, component);
-                }
-            }
-            if (CONFIG.iconTooltips.honeyTooltip && itemStack.has(DataComponents.BLOCK_STATE)) {
-                var state = itemStack.get(DataComponents.BLOCK_STATE);
-                if (state == null) return;
-                var honey = state.get(BeehiveBlock.HONEY_LEVEL);
-                if (honey == null) return;
-                addIcon(ResourceLocation.withDefaultNamespace("honey"), honey, tooltipFlag, list, component);
-            }
+            addAttributeTooltips(itemStack, list, component);
+            addBeeTooltips(itemStack, list, component);
+            addFuelTooltips(itemStack, list, component);
+            addHoneyTooltips(itemStack, list, component);
             // Add icon
             if (!component.equals(Component.empty()))
                 list.add(component);
         }
+        // Add text tooltips
         if (CONFIG.textTooltips.lodestoneTooltip && itemStack.has(DataComponents.LODESTONE_TRACKER)) {
             var state = itemStack.get(DataComponents.LODESTONE_TRACKER);
             if (state == null || state.target().isEmpty()) return;
@@ -149,7 +94,79 @@ public class InlineTooltips {
         }
     }
 
-    private static void addIcon(ResourceLocation attribute, double amount, TooltipFlag tooltipFlag, List<Component> list, MutableComponent component) {
+    private static void addAttributeTooltips(ItemStack itemStack, List<Component> list, MutableComponent component) {
+        if (CONFIG.iconTooltips.attributeTooltips) {
+            for (EquipmentSlotGroup equipmentSlotGroup : EquipmentSlotGroup.values()) {
+                //? if >1.21.8 {
+                itemStack.forEachModifier(equipmentSlotGroup, (holder, attributeModifier, display) -> {
+                    //?} else {
+                    /*itemStack.forEachModifier(equipmentSlotGroup, (holder, attributeModifier) -> {
+                     *///?}
+                    //? if >1.21.8
+                    if (display != ItemAttributeModifiers.Display.hidden()) {
+                        var player = Minecraft.getInstance().player;
+                        AtomicReference<Double> amount = new AtomicReference<>(attributeModifier.amount());
+                        if (player != null) {
+                            amount.set(switch (attributeModifier.operation()) {
+                                case ADD_VALUE -> attributeModifier.amount() + player.getAttributeBaseValue(holder);
+                                case ADD_MULTIPLIED_BASE, ADD_MULTIPLIED_TOTAL ->
+                                        attributeModifier.amount() * player.getAttributeBaseValue(holder);
+                            });
+                        }
+                        amount.set(SharpnessHelpers.addSharpnessDamage(itemStack, amount.get(), player, attributeModifier));
+                        var icon = holder.unwrapKey().orElseThrow().location();
+                        if (amount.get()!=0)
+                            addIcon(icon, amount.get(), list, component);
+                        //? if >1.21.8
+                    }
+                });
+            }
+        }
+    }
+
+    private static void addBeeTooltips(ItemStack itemStack, List<Component> list, MutableComponent component) {
+        if (itemStack.has(DataComponents.BEES) && CONFIG.iconTooltips.beesTooltip) {
+            var bees = itemStack.get(DataComponents.BEES);
+            if (bees == null) return;
+            addIcon(ResourceLocation.withDefaultNamespace("bees"), bees
+                    //? if >1.21.8
+                    .bees()
+                    .size(), list, component);
+        }
+    }
+
+    private static void addFuelTooltips(ItemStack itemStack, List<Component> list, MutableComponent component) {
+        if (CONFIG.iconTooltips.fuelTooltip) {
+            var level = Minecraft.getInstance().level;
+            if (level != null &&
+                    //? if >1.21.8 {
+                    level.fuelValues()
+                            //?} else {
+                            /*AbstractFurnaceBlockEntity
+                             *///?}
+                            .isFuel(itemStack)) {
+                addIcon(ResourceLocation.withDefaultNamespace("fuel"),
+                        //? if >1.21.8 {
+                        level.fuelValues().burnDuration(itemStack)
+                                //?} else {
+                                /*AbstractFurnaceBlockEntity.getFuel().get(itemStack.getItem())
+                                 *///?}
+                                /200f, list, component);
+            }
+        }
+    }
+
+    private static void addHoneyTooltips(ItemStack itemStack, List<Component> list, MutableComponent component) {
+        if (CONFIG.iconTooltips.honeyTooltip && itemStack.has(DataComponents.BLOCK_STATE)) {
+            var state = itemStack.get(DataComponents.BLOCK_STATE);
+            if (state == null) return;
+            var honey = state.get(BeehiveBlock.HONEY_LEVEL);
+            if (honey == null) return;
+            addIcon(ResourceLocation.withDefaultNamespace("honey"), honey, list, component);
+        }
+    }
+
+    private static void addIcon(ResourceLocation attribute, double amount, List<Component> list, MutableComponent component) {
         //? if >1.21.8 {
         ResourceLocation icon = ResourceLocation.fromNamespaceAndPath(attribute.getNamespace(), "inline_tooltip_icons/"+ attribute.getPath());
         MutableComponent iconComponent = Component.object(new AtlasSprite(AtlasSprite.DEFAULT_ATLAS, icon));
